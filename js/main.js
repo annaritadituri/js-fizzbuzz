3c52e6d67787aebdd40fc9e32c389431a2b49e49
annaritadituri <annaritadituri@icloud.com>
2024-05-04 19:09:27 +0200

    BONUS 1

--- js/main.js
+++ js/main.js
@@ -2,14 +2,27 @@
 
 //////////////////////////////////////////////////////////
 
+let ul = document.querySelector('ul');
+
 for (let i = 1; i <= 100; i++) {
+
+    let li = document.createElement('li');
+
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
+        li.append("FizzBuzz");
+        ul.append(li);
     } else if (i % 3 === 0) {
         console.log("Fizz");
+        li.append("Fizz");
+        ul.append(li);
     } else if (i % 5 === 0) {
         console.log("Buzz");
+        li.append("Buzz");
+        ul.append(li);
     } else {
         console.log(i);
+        li.append(i);
+        ul.append(li);
     }
 }
